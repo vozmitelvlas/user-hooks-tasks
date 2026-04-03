38651abf06a3811668a50ace611292e121c2c88e
vozmitelvlas <vozmitel.vlas228@mail.ru>
2026-04-03 13:55:05 +0300

    create useHover hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useHover } from './hooks';
 import './App.css';
 
 function App() {
+  const { hovered, ref } = useHover<HTMLDivElement>();
 
   return (
     <div className='tasks'>
+      <div ref={ref} className='task3'>
+        {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
+      </div>
     </div>
   );
 }
