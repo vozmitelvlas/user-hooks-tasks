96b8ef8f62af1ad92b4217ef66aec2547521cd93
vozmitelvlas <vozmitel.vlas228@mail.ru>
2026-04-03 15:38:34 +0300

    feat: create useViewPortSize hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useViewportSize } from './hooks';
 import './App.css';
 
 function App() {
+  const { height, width } = useViewportSize();
 
   return (
     <div className='tasks'>
+      <div className='task3'>
+        Width: {width}, height: {height}
+      </div>
     </div>
   );
 }
